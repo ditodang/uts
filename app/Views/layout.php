b11660da16ac9ab0ab69603d08b7fceceb5a2d9f
azeebs <artdimensi21@gmail.com>
2023-05-15 14:23:23 +0700

    first

---
+++ app/Views/layout.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+
+    <!--- basic page needs
+   ================================================== -->
+    <meta charset="utf-8">
+    <title>Dito dang kurniawan</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+   ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+   ================================================== -->
+    <link rel="stylesheet" href="<?php echo base_url('cvd/css/base.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('cvd/css/main.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('cvd/css/vendor.css') ?>">
+
+    <!-- script
+   ================================================== -->
+    <script src="<?php echo base_url('cvd/js/modernizr.js') ?>"></script>
+    <script src="<?php echo base_url('cvd/js/pace.min.js') ?>"></script>
+
+    <!-- favicons
+	================================================== -->
+    <link rel="icon" type="image/png" href="favicon.png">
+
+</head>
+
+<body id="top">
+
+    <!-- header 
+   ================================================== -->
+    <header>
+        <div class="row">
+
+            <div>
+                <a class="menu-toggle" href="#"><span>Menu</span></a>
+
+                <div>
+
+                </div>
+
+                <nav id="main-nav-wrap">
+                    <ul class="main-navigation">
+                        <li class="current"><a class="smoothscroll" href="#intro" title="">Home</a></li>
+                        <li><a class="smoothscroll" href="#about" title="">About</a></li>
+                        <li><a class="smoothscroll" href="#contact" title="">Contact</a></li>
+                    </ul>
+                </nav>
+            </div> <!-- /top-bar -->
+
+        </div> <!-- /row -->
+    </header> <!-- /header -->
+
+    <!-- intro section
+   ================================================== -->
+    <section id="intro">
+
+        <div class="intro-overlay"></div>
+
+        <div class="intro-content">
+            <div class="row">
+
+                <div class="col-twelve">
+                    <h1>Dito Dang Kurniawan</h1>
+                    <a class="button stroke smoothscroll" href="#about" title="">Tentang saya</a>
+
+                </div>
+
+            </div>
+        </div> <!-- /intro-content -->
+
+        <ul class="intro-social">
+            <li><a href="https://github.com/ditodang/"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://instagram.com/dito_k98"><i class="fa fa-instagram"></i></a></li>
+        </ul> <!-- /intro-social -->
+
+    </section> <!-- /intro -->
+
+
+    <!-- about section
+   ================================================== -->
+    <section id="about" class="grey-section">
+
+        <div class="row section-intro">
+            <div class="col-twelve">
+
+
+                <h1>Curriculum Vitae <br>Dito Dang kurniawan</h1>
+
+                <div class="intro-info">
+
+                    <img src="<?php echo base_url('cvd/images/profile-pic.jpg') ?>" alt="Profile Picture">
+
+                </div>
+
+            </div>
+        </div> <!-- /section-intro -->
+
+        <div class="row about-content">
+
+            <div class="col-six tab-full">
+                <h3>Profile</h3>
+                <p class="lead">
+                    Saya sedang kuliah di Universitas Muhammadiyah Sukabumi mengambil program studi Teknik Informatika Semester 6, Saya termasuk kedalam orang yang rajin menabung
+                </p>
+                <ul class="info-list">
+                    <li>
+                        <strong>Nama Lengkap:</strong>
+                        <span>Dito Dang Kurniawan</span>
+                    </li>
+                    <li>
+                        <strong>Tempat, Tanggal lahir:</strong>
+                        <span>Sukabumi, 21-April-2002</span>
+                    </li>
+                    <li>
+                        <strong>Alamat:</strong>
+                        <span>Kp.cipendeuy </span>
+                    </li>
+                    <li>
+                        <strong>Umur:</strong>
+                        <span>21</span>
+                    </li>
+                    <li>
+                        <strong>Agama:</strong>
+                        <span>Islam</span>
+                    </li>
+                    <li>
+                    <li>
+                        <strong>Status:</strong>
+                        <span>Belum Menikah</span>
+                    </li>
+                    <li>
+                        <strong>Email:</strong>
+                        <span>ditodangkurniawan03@gmail.com </span>
+                    </li>
+
+                </ul> <!-- /info-list -->
+
+            </div>
+
+            <div class="col-six tab-full">
+
+                <h3>Pendidikan</h3>
+                <ul class="info-list">
+                    <li>
+                        <strong>SMA N 1 Padang bolak</strong>
+                        <span>2018-2020</span>
+                    </li>
+                    <li>
+                        <strong>Universitas Muhammadiyah Sukabumi</strong>
+                        <span>2020-Sekarang</span>
+                </ul>
+
+                <h3>Skills</h3>
+
+                <ul class="skill-bars">
+                    <li>
+                        <div class="progress percent80"><span>80%</span></div>
+                        <strong>HTML</strong>
+                    </li>
+                    <li>
+                        <div class="progress percent80"><span>80%</span></div>
+                        <strong>CSS</strong>
+                    </li>
+                    <li>
+                        <div class="progress percent80"><span>80%</span></div>
+                        <strong>PHP</strong>
+                    </li>
+
+                    <h3>Soft skills</h3>
+                    <ul class="skill-bars">
+                        <li>
+                            <div class="progress percent90"><span>90%</span></div>
+                            <strong>Team Work</strong>
+                        </li>
+                        <li>
+                            <div class="progress percent85"><span>85%</span></div>
+                            <strong>Problem Solving</strong>
+                        </li>
+                        <li>
+                            <div class="progress percent80"><span>80%</span></div>
+                            <strong>Time management</strong>
+                        </li>
+
+
+
+
+                    </ul> <!-- /skill-bars -->
+
+            </div>
+
+        </div>
+
+        <div class="row button-section">
+            <div class="col-twelve">
+                <a href="#contact" title="Kontak" class="button stroke smoothscroll">Kontak</a>
+
+            </div>
+        </div>
+
+    </section> <!-- /process-->
+
+
+    <!-- contact
+   ================================================== -->
+    <section id="contact">
+
+        <div class="row section-intro">
+            <div class="col-twelve">
+
+                <h5>Kontak</h5>
+                <h1>Hubungi Saya</h1>
+            </div>
+        </div> <!-- /section-intro -->
+
+        <div class="row contact-form">
+
+            <div class="col-twelve">
+
+                <!-- form -->
+                <form name="contactForm" id="contactForm" method="post" action="">
+                    <fieldset>
+
+                        <div class="form-field">
+                            <input name="contactName" type="text" id="contactName" placeholder="Nama" value="" minlength="2" required="">
+                        </div>
+                        <div class="form-field">
+                            <input name="contactEmail" type="email" id="contactEmail" placeholder="Email" value="" required="">
+                        </div>
+                        <div class="form-field">
+                            <input name="contactSubject" type="text" id="contactSubject" placeholder="Subject" value="">
+                        </div>
+                        <div class="form-field">
+                            <textarea name="contactMessage" id="contactMessage" placeholder="Pesan" rows="10" cols="50" required=""></textarea>
+                        </div>
+                        <div class="form-field">
+                            <button class="submitform">Kirim</button>
+                            <div id="submit-loader">
+                                <div class="text-loader">Sending...</div>
+                                <div class="s-loader">
+                                    <div class="bounce1"></div>
+                                    <div class="bounce2"></div>
+                                    <div class="bounce3"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </fieldset>
+                </form> <!-- Form End -->
+
+                <!-- contact-warning -->
+                <div id="message-warning">
+                </div>
+                <!-- contact-success -->
+                <div id="message-success">
+                    <i class="fa fa-check"></i>Terkirim<br>
+                </div>
+
+            </div> <!-- /col-twelve -->
+
+        </div> <!-- /contact-form -->
+
+        <div class="row contact-info">
+
+            <div class="col-four tab-full">
+
+                <div class="icon">
+                    <i class="icon-pin"></i>
+                </div>
+
+                <h5>Lokasi</h5>
+
+                <p>
+                    Kebon Manggu,<br>
+                    Gunung Guruh<br>
+                </p>
+
+            </div>
+
+            <div class="col-four tab-full collapse">
+
+                <div class="icon">
+                    <i class="icon-mail"></i>
+                </div>
+
+                <h5>Email</h5>
+
+                <p>ditodangkurniawan@gmail.com<br>
+                </p>
+
+            </div>
+
+            <div class="col-four tab-full">
+
+                <div class="icon">
+                    <i class="icon-phone"></i>
+                </div>
+
+                <h5>Handphone</h5>
+
+                <p>082277978190<br>
+                </p>
+
+            </div>
+
+        </div> <!-- /contact-info -->
+
+    </section> <!-- /contact -->
+
+
+    <!-- footer
+   ================================================== -->
+
+    <footer>
+        <div class="row">
+
+            <div class="col-six tab-full pull-right social">
+
+                <ul class="footer-social">
+
+                    <li><a href="https://github.com/ditodangs/"><i class="fa fa-github"></i></a></li>
+                    <li><a href="https://instagram.com/dito_k98"><i class="fa fa-instagram"></i></a></li>
+
+                </ul>
+
+            </div>
+
+            <div id="go-top">
+                <a class="smoothscroll" title="Back to Top" href="#top"><i class="fa fa-long-arrow-up"></i></a>
+            </div>
+
+        </div> <!-- /row -->
+    </footer>
+
+    <div id="preloader">
+        <div id="loader"></div>
+    </div>
+
+    <!-- Java Script
+   ================================================== -->
+    <script src="<?php echo base_url('cvd/js/jquery-2.1.3.min.js') ?>"></script>
+    <script src="<?php echo base_url('cvd/js/plugins.js') ?>"></script>
+    <script src="<?php echo base_url('cvd/js/main.js') ?>"></script>
+
+</body>
+
+</html>
